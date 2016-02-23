681a05db822c76d4e75fa200db5911d02c447746
Alexander Frewein <alexander.frewein@student.tugraz.at>
2016-02-23 13:46:41 +0100

    New integer_factorization

--- integer_factorization.c
+++ integer_factorization.c
@@ -1,44 +1,63 @@
 #include<stdio.h>
+#include<stdlib.h>
+#define FALSE 0
+#define TRUE 1
 
 
 
 int main()
+
 {
-while(1)
-{
- int primzahl = 2;
- int number;
- int array[20];
- int i = 0;
- printf("Geben Sie eine Zahl ein:\n");
- scanf("%d", &number);
 
 
- if( (number % primzahl) == 0)
- {
-  if( (number / primzahl) == 1)
+
+  while(1)
   {
-    array[i] = primzahl;
-    printf("Primzahl:%d\n", array[i]);
-    i++;
-    primzahl++;
+    int number = 0;
+
+    int boolean = FALSE;
+
+    int primzahl_checker = 2;
+    int no_primzahl = 0;
+
+    printf("Geben Sie eine Zahl ein!\n");
+    scanf("%d", &number);
+
+      while(1)
+      {
+
+        if((primzahl_checker == number)  || (primzahl_checker == no_primzahl))
+        {
+          break;
+        }
+
+        if( (number % primzahl_checker) == 0)
+        {
+          no_primzahl = number / primzahl_checker;
+          printf("%d\t", no_primzahl);
+           primzahl_checker++;
+           boolean = TRUE;
+        }
+
+        else{
+          primzahl_checker++;
+        }
+
+
+      }
+
+    if(boolean == FALSE)
+    {
+      printf("\nPrimzahl ist: %d\n", number);
+    }
+
+    if(boolean == TRUE)
+    {
+      printf("\nPrimzahl ist: %d\n", no_primzahl);
+    }
+
 
   }
-
- }
-
- else
- {
-    primzahl++;
-
- }
-
-
-}
-
-
-
-
 
 
 
